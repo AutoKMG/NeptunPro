889e69dd4e05119ead478169fbbb20f48a1a0550
Khaled Saleh <th.be03@gmail.com>
2023-10-28 23:34:01 +0200

    fix: allowing frontend domain in security

--- backend/src/main/java/hu/unideb/inf/NeptunPro/security/SecurityConfiguration.java
+++ backend/src/main/java/hu/unideb/inf/NeptunPro/security/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -23,7 +24,15 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configure(http))
+                .cors(cors -> {
+                    cors.configurationSource(request -> {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.addAllowedOrigin("http://localhost:8081");
+                        config.addAllowedMethod("*");
+                        config.addAllowedHeader("*");
+                        return config;
+                    });
+                })
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/api/auth/**").permitAll();
                     auth.requestMatchers("/api/**").fullyAuthenticated();
